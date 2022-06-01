51608cec90a61b2afd3411131f66ad58fc33bfe0
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-06-02 00:40:43 +0300

    фвв Ðadd dishes-list and dishes-item component

---
+++ src/app/components/dishes-list/dishes-item/dishes-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dishes-item',
+  templateUrl: './dishes-item.component.html',
+  styleUrls: ['./dishes-item.component.scss']
+})
+export class DishesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
